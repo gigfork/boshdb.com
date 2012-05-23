fead456554976604252d58cdcce3bc465510a129
Brian McClain <brianmmcclain@gmail.com>
2012-05-22 23:10:22 -0400

    Rails boilerplate code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boshdb::Application.config.secret_token = '93ee4557f16d6df0fddf6a4056da3ffb7aa7790ea5b0d98ab4de52815d138a2364eb30d6a5cccc3984d1c07c11583ddbf5fa32f15c3b4be93206c3c356c7fede'
