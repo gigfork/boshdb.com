7630a8d3bf15d5cb2b0dea9d726b6758b00fc0f2
Brian McClain <brianmmcclain@gmail.com>
2012-06-23 01:01:21 -0400

    Fixed rating logic

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,12 +1,13 @@
 class RatingsController < ApplicationController
   
   def rate
-    release_id = params[:release]
-    rating_val = params[:rating]
+    release_id = params[:release].to_f
+    rating_val = params[:rating].to_f
+    
+    @release = Release.find_by_id(release_id)
     
     if user_signed_in?
       # User signed in, let's make sure that they havn't voted on this release yet
-      @release = Release.find_by_id(release_id)
       existing_rating = nil
       @release.ratings.each do |rating|
         if rating.user.id == current_user.id
@@ -16,12 +17,9 @@ class RatingsController < ApplicationController
       
       if existing_rating
         # Has an existing rating, modify current vote
-        @release.rating -= existing_rating.rating
         existing_rating.rating = rating_val
         existing_rating.save
         @rating = existing_rating
-        @release.rating += @rating.rating
-        @release.save
       else
         # No existing rating, make a new one
         @rating = Rating.new
@@ -29,11 +27,26 @@ class RatingsController < ApplicationController
         @rating.user_id = current_user.id
         @rating.rating = rating_val
         @rating.save
-        @release.rating += @rating.rating
-        @release.save
       end
       
+      total_score = 0
+      total_votes = 0
+      ratings = Rating.find_all_by_release_id(@release.id)
+      puts ratings.inspect
+      if ratings.kind_of?(Array)
+        ratings.each do |r|
+         puts r.inspect
+         total_score += r.rating
+         total_votes += 1
+        end
+      else
+        total_score = ratings.rating
+        total_votes = 1
+      end
     end
+    
+    @release.rating = (total_score.to_f / total_votes.to_f).round(2)
+    @release.save
     
     redirect_to @release
     
