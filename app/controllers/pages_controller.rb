29c428b0c016114cac3ca56f5315e62b7967025e
Brian McClain <brianmmcclain@gmail.com>
2012-07-04 01:08:18 -0400

    Show top 5 and 5 newest releases on index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
 class PagesController < ApplicationController
-  
+  def index
+    @releases_top5 = Release.order("rating").limit(5)
+    @releases_newest = Release.order("created_at DESC").limit(5)
+  end
 end
