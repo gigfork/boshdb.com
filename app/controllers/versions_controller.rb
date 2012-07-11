1e96abd13012595eaf3856b9f0958d26007ba26f
Brian McClain <brianmmcclain@gmail.com>
2012-07-10 22:56:07 -0400

    Added check for valid URL, and try to get tarbal from GitHub source URL

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,3 +1,5 @@
+require 'URI'
+
 class VersionsController < ApplicationController
 
   before_filter :authenticate_user!, :except => [:download]
@@ -8,11 +10,45 @@ class VersionsController < ApplicationController
     
     @version = Version.where('version_number = ? AND release_id = ?', version_number, release_id).first
     
-    # Increase the download count
-    @version.downloads += 1
-    @version.save
+    # Test to make sure that the download URL is valid
+    valid_url = false
+    download_url = ""
+    begin
+      url = URI.parse(@version.download_url)
+      if url.scheme != nil and url.host != nil
+        valid_url = true
+        download_url = @version.download_url
+      end
+    rescue
+    end
     
-    redirect_to @version.download_url
+    # If it's not a valid URL, let's see if it's a GitHub source URL, and work from there
+    if !valid_url
+      begin
+        url = URI.parse(@version.release.source_url)
+        if url.host.downcase == "github.com"
+          github_url = @version.release.source_url
+          if !github_url.ends_with?("/")
+            github_url += "/"
+          end
+          github_url += "tarball/master"
+          valid_url = true
+          download_url = github_url
+        end
+      rescue
+      end
+    end
+    
+    if valid_url
+      # We found a valid download URL
+      # Increase the download count
+      @version.downloads += 1
+      @version.save
+    
+      redirect_to download_url
+    else
+      # No valid download URL could be determined, throw and error and redirect
+    end
   end
   
   # POST /versions
