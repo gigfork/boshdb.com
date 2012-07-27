3e390d4f9ed438e4944d04060d60cb3fbf9c3690
Brian McClain <brianmmcclain@gmail.com>
2012-07-26 23:30:33 -0400

    Fixed Top 5 sorting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @releases_top5 = Release.order("rating").limit(5)
+    @releases_top5 = Release.order("rating DESC").limit(5)
     @releases_newest = Release.order("created_at DESC").limit(5)
   end
   
